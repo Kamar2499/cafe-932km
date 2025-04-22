b802441c9fc2d4e0ae7a25e76c9e0daa79e9f75c
Kamar2499 <varazdat2499@gmail.com>
2025-04-23 02:11:23 +0300

    астройка GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  basePath: '/cafe-932km',
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
+    unoptimized: true,
   },
 }
 
