ea10d0088a038f4fe6d290765d48e1e28b418607
Kamar2499 <varazdat2499@gmail.com>
2025-04-23 03:22:26 +0300

    fix: update Next.js config and GitHub Actions workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: 'out',
   basePath: '/cafe-932km',
   images: {
     unoptimized: true,
+    remotePatterns: []
   },
   trailingSlash: true,
   reactStrictMode: true,
